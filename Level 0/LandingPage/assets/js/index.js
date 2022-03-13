d78a3c901d1a4672b49bfadd64016cb6d280e858
soan27 <hoangthixoan272000@gmail.com>
2022-03-13 23:05:48 +0700

    Landing Page

---
+++ Level 0/LandingPage/assets/js/index.js
@@ -0,0 +1,8 @@
+const servic = document.querySelector("#tag-services");
+const servicesTarget = document.querySelector("#services");
+
+servic.addEventListener("click", function() {
+    servicesTarget.scrollIntoView({
+        behavior: "smooth",
+    });
+});
