cb653d302afb251607ef0872571de453ed8961e4
soan27 <hoangthixoan272000@gmail.com>
2022-03-13 23:08:52 +0700

    edit

--- Level 0/LandingPage/assets/js/index.js
+++ Level 0/LandingPage/assets/js/index.js
@@ -1,8 +1,8 @@
-const servic = document.querySelector("#tag-services");
-const servicesTarget = document.querySelector("#services");
+// const servic = document.querySelector("#tag-services");
+// const servicesTarget = document.querySelector("#services");
 
-servic.addEventListener("click", function() {
-    servicesTarget.scrollIntoView({
-        behavior: "smooth",
-    });
-});
+// servic.addEventListener("click", function() {
+//     servicesTarget.scrollIntoView({
+//         behavior: "smooth",
+//     });
+// });
